ee4b50de949e918cc4e872fe09d0cfd62d0606fd
Shiva Bajpai <shivabajpai2006@gmail.com>
2026-02-27 12:52:30 +0530

    Refactor components

--- vite.config.js
+++ vite.config.js
@@ -16,3 +16,30 @@ export default defineConfig({
         theme_color: '#0a0a0f',
         background_color: '#0a0a0f',
         display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: "maskable"
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      }
